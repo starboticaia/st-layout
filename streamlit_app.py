5fef7a4a05cbdba0e47e25be240e9fa66eb24c33
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-05-02 11:51:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,16 @@ from PIL import Image
 
 fichero = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
-original = Image.open(fichero)
-img_array = np.array(original)
-
-col1, col2 = st.columns(2)
-
-#original = Image.open(image)
-col1.header("Original")
-col1.image(original, use_column_width=True)
-
-gris = original.convert('LA')
-col2.header("Escala de grises")
-col2.image(gris, use_column_width=True)
+if fichero is not None:
+  original = Image.open(fichero)
+  img_array = np.array(original)
+  
+  col1, col2 = st.columns(2)
+  
+  #original = Image.open(image)
+  col1.header("Original")
+  col1.image(original, use_column_width=True)
+  
+  gris = original.convert('LA')
+  col2.header("Escala de grises")
+  col2.image(gris, use_column_width=True)
