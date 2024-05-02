73ce9ce07ff606aba8f216ebf4c07209a463ddf9
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-05-02 12:28:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,15 @@ import numpy as np
 from PIL import Image
 
 fichero = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+rotada = None
+angulo = st.slider('ángulo',0,360)
+  
 
 if fichero is not None:
-  original = Image.open(fichero)
+  original = Image.open(fichero)  
   #img_array = np.array(original)
-  
-  col1, col2 = st.columns(2)
+
+  col1, col2, col3 = st.columns(3)
     
   col1.header("Original")
   col1.image(original, use_column_width=True)
@@ -17,8 +20,6 @@ if fichero is not None:
   col2.header("Escala de grises")
   col2.image(gris, use_column_width=True)
 
-  st.divider()
-  #ancho, alto = img_array.size
-  angulo = st.slider('ángulo',0,360)
-  original = original.rotate(angulo)
-  
+  rotada = original.rotate(angulo)
+  col3.header("Rotación")
+  col3.image(rotada, use_column_width=True)
