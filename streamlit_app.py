cab255baa264dc565d2ad251b1619a3754c91ec7
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-05-02 11:43:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write("Hola mundo")
+col1, col2 = st.columns(2)
+
+original = Image.open(image)
+col1.header("Original")
+col1.image(original, use_column_width=True)
+
+gris = original.convert('LA')
+col2.header("Escala de grises")
+col2.image(grayscale, use_column_width=True)
